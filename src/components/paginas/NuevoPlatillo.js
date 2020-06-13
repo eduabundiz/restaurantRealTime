7ca306650459fc502e60ba7a8937cb59f619260e
Abundiz <3duabundiz@gmail.com>
2020-06-13 15:05:43 -0500

    mostrar mensajes de error en formulario

--- src/components/paginas/NuevoPlatillo.js
+++ src/components/paginas/NuevoPlatillo.js
@@ -49,8 +49,15 @@ const NuevoPlatillo = () => {
                             placeholder="Nombre Platillo"
                             value= {formik.values.nombre}
                             onChange = {formik.handleChange}
+                            onBlur = {formik.handleBlur}
                         />
                     </div>
+                    { formik.touched.nombre && formik.errors.nombre ?(
+                        <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-5" role="alert">
+                            <p className="font-bold">Hubo un error</p>
+                            <p>{formik.errors.nombre}</p>
+                        </div>
+                    ) : null}
 
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="precio" >Precio</label>
@@ -62,10 +69,16 @@ const NuevoPlatillo = () => {
                             min="0"
                             value= {formik.values.precio}
                             onChange = {formik.handleChange}
-                            
+                            onBlur = {formik.handleBlur}
                         />
                     </div>
-
+                    { formik.touched.precio && formik.errors.precio ?(
+                        <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-5" role="alert">
+                            <p className="font-bold">Hubo un error</p>
+                            <p>{formik.errors.precio}</p>
+                        </div>
+                    ) : null}
+                    
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="categoria" >Categoria</label>
                         <select
@@ -74,6 +87,7 @@ const NuevoPlatillo = () => {
                             name="categoria"
                             value= {formik.values.categoria}
                             onChange = {formik.handleChange}
+                            onBlur = {formik.handleBlur}
                         >
                             <option value="">-- Sellecione --</option>
                             <option value="desayuno">Desayuno</option>
@@ -84,6 +98,12 @@ const NuevoPlatillo = () => {
                             <option value="ensaladas">Ensaladas</option>
                         </select>
                     </div>
+                    { formik.touched.categoria && formik.errors.categoria ?(
+                        <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-5" role="alert">
+                            <p className="font-bold">Hubo un error</p>
+                            <p>{formik.errors.categoria}</p>
+                        </div>
+                    ) : null}
 
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="imagen" >Imagen</label>
@@ -93,6 +113,7 @@ const NuevoPlatillo = () => {
                             type="file"
                             value= {formik.values.imagen}
                             onChange = {formik.handleChange}
+                            onBlur = {formik.handleBlur}
                         />
                     </div>
 
@@ -105,8 +126,15 @@ const NuevoPlatillo = () => {
                             min="0"
                             value= {formik.values.descripcion}
                             onChange = {formik.handleChange}
+                            onBlur = {formik.handleBlur}
                         ></textarea>
                     </div>
+                    { formik.touched.descripcion && formik.errors.descripcion ?(
+                        <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-5" role="alert">
+                            <p className="font-bold">Hubo un error</p>
+                            <p>{formik.errors.descripcion}</p>
+                        </div>
+                    ) : null}
 
                     <input
                         type="submit"
